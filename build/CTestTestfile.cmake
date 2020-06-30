4f224bb2bb6aa31a6217b937c6d12dacb068582d
humbertleonardo <57204453+humbertleonardo@users.noreply.github.com>
2020-06-30 12:44:23 -0300

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/humbert/Arquivos/Trabalhos/Algoritmos II/PacmanCovid
+# Build directory: /home/humbert/Arquivos/Trabalhos/Algoritmos II/PacmanCovid/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

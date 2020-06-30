eb43b633559324100ce290d67172e9cdb9519164
humbertleonardo <57204453+humbertleonardo@users.noreply.github.com>
2020-06-30 12:58:17 -0300

    Add files via upload

---
+++ build/CMakeFiles/PacmanCovid.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PacmanCovid.dir/main.cpp.o"
+  "CMakeFiles/PacmanCovid.dir/lib/libraries.cpp.o"
+  "PacmanCovid.pdb"
+  "PacmanCovid"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PacmanCovid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
